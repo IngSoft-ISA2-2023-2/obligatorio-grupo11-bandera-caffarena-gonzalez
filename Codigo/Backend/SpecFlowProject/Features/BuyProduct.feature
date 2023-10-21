7c26d0b88b635a0b75433acd5bb35b1ea5cb47bf
Sebastián Bandera <sebastianbandera17@gmail.com>
2023-10-21 17:31:08 -0300

    RED - Compra de producto

---
+++ Codigo/Backend/SpecFlowProject/Features/BuyProduct.feature
@@ -0,0 +1,38 @@
+﻿Feature: BuyProduct
+
+@mytag
+Scenario: Correcta compra de un producto único
+	Given que en el escenario_I, selecciono un producto con una unidad
+	When en el escenario_I, presiono el botón de añadir al carrito y finalizo la compra
+	Then en el escenario_I, se compra el producto correctamente
+
+@mytag
+Scenario: Correcta compra de un producto con varias unidades
+	Given que en el escenario_II, selecciono un producto con dos unidades
+	When en el escenario_II, presiono el botón de añadir al carrito y finalizo la compra
+	Then en el escenario_II, se compra las dos unidades del producto correctamente
+
+@mytag
+Scenario: Correcta compra de dos productos distintos de la misma farmacia
+	Given que en el escenario_III, selecciono un producto con dos unidades, y otro producto de la misma farmacia con una unidad
+	When en el escenario_III, presiono el botón de añadir al carrito y finalizo la compra
+	Then en el escenario_III, se compra las tres unidades de los productos correctamente
+
+@mytag
+Scenario: Correcta compra de dos productos distintos de distinta farmacia
+	Given que en el escenario_IV, selecciono un producto con dos unidades, y otro producto de distinta farmacia con una unidad
+	When en el escenario_IV, presiono el botón de añadir al carrito y finalizo la compra
+	Then en el escenario_IV, se compra las tres unidades de los productos correctamente
+
+@mytag
+Scenario: Correcta compra de productos combinado con medicamento
+	Given que en el escenario_V, selecciono un producto y un medicamento
+	When en el escenario_V, presiono el botón de añadir al carrito y finalizo la compra
+	Then en el escenario_V, se compra del producto y medicamentos se realiza correctamente.
+
+@mytag
+Scenario: Intento de compra de producto fallido por monto negativo
+	Given que en el escenario_VI, selecciono un producto con cantidad negativa
+	When en el escenario_VI, presiono el botón de añadir al carrito e intento finalizar la compra
+	Then en el escenario_VI, no se compra del producto por monto negativo.
+
---
+++ Codigo/Backend/SpecFlowProject/StepDefinitions/BuyProductStepDefinitions.cs
@@ -0,0 +1,117 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.StepDefinitions
+{
+    [Binding]
+    public class BuyProductStepDefinitions
+    {
+        [Given(@"que en el escenario_I, selecciono un producto con una unidad")]
+        public void GivenQueEnElEscenario_ISeleccionoUnProductoConUnaUnidad()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_I, presiono el botón de añadir al carrito y finalizo la compra")]
+        public void WhenEnElEscenario_IPresionoElBotonDeAnadirAlCarritoYFinalizoLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_I, se compra el producto correctamente")]
+        public void ThenEnElEscenario_ISeCompraElProductoCorrectamente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_II, selecciono un producto con dos unidades")]
+        public void GivenQueEnElEscenario_IISeleccionoUnProductoConDosUnidades()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_II, presiono el botón de añadir al carrito y finalizo la compra")]
+        public void WhenEnElEscenario_IIPresionoElBotonDeAnadirAlCarritoYFinalizoLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_II, se compra las dos unidades del producto correctamente")]
+        public void ThenEnElEscenario_IISeCompraLasDosUnidadesDelProductoCorrectamente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_III, selecciono un producto con dos unidades, y otro producto de la misma farmacia con una unidad")]
+        public void GivenQueEnElEscenario_IIISeleccionoUnProductoConDosUnidadesYOtroProductoDeLaMismaFarmaciaConUnaUnidad()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_III, presiono el botón de añadir al carrito y finalizo la compra")]
+        public void WhenEnElEscenario_IIIPresionoElBotonDeAnadirAlCarritoYFinalizoLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_III, se compra las tres unidades de los productos correctamente")]
+        public void ThenEnElEscenario_IIISeCompraLasTresUnidadesDeLosProductosCorrectamente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_IV, selecciono un producto con dos unidades, y otro producto de distinta farmacia con una unidad")]
+        public void GivenQueEnElEscenario_IVSeleccionoUnProductoConDosUnidadesYOtroProductoDeDistintaFarmaciaConUnaUnidad()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_IV, presiono el botón de añadir al carrito y finalizo la compra")]
+        public void WhenEnElEscenario_IVPresionoElBotonDeAnadirAlCarritoYFinalizoLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_IV, se compra las tres unidades de los productos correctamente")]
+        public void ThenEnElEscenario_IVSeCompraLasTresUnidadesDeLosProductosCorrectamente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_V, selecciono un producto y un medicamento")]
+        public void GivenQueEnElEscenario_VSeleccionoUnProductoYUnMedicamento()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_V, presiono el botón de añadir al carrito y finalizo la compra")]
+        public void WhenEnElEscenario_VPresionoElBotonDeAnadirAlCarritoYFinalizoLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_V, se compra del producto y medicamentos se realiza correctamente\.")]
+        public void ThenEnElEscenario_VSeCompraDelProductoYMedicamentosSeRealizaCorrectamente_()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_VI, selecciono un producto con cantidad negativa")]
+        public void GivenQueEnElEscenario_VISeleccionoUnProductoConCantidadNegativa()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_VI, presiono el botón de añadir al carrito e intento finalizar la compra")]
+        public void WhenEnElEscenario_VIPresionoElBotonDeAnadirAlCarritoEIntentoFinalizarLaCompra()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_VI, no se compra del producto por monto negativo\.")]
+        public void ThenEnElEscenario_VINoSeCompraDelProductoPorMontoNegativo_()
+        {
+            throw new PendingStepException();
+        }
+    }
+}

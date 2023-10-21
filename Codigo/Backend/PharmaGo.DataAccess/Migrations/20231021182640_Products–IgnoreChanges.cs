f8f06a864af0ce35169f873df99e7b3365c33f52
Martin Caffarena <martincaffarena@gmail.com>
2023-10-21 15:30:48 -0300

    ADD - Migracion BD

---
+++ Codigo/Backend/PharmaGo.DataAccess/Migrations/20231021182640_Products–IgnoreChanges.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PharmaGo.DataAccess.Migrations
+{
+    public partial class ProductsIgnoreChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pruducts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false),
+                    PharmacyId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pruducts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pruducts_Pharmacys_PharmacyId",
+                        column: x => x.PharmacyId,
+                        principalTable: "Pharmacys",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pruducts_PharmacyId",
+                table: "Pruducts",
+                column: "PharmacyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pruducts");
+        }
+    }
+}

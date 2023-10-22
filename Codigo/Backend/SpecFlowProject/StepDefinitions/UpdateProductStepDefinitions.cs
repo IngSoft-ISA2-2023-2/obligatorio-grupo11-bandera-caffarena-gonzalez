331ca93adc953b90559c0491502711f7f2bfc064
Sebastián Bandera <sebastianbandera17@gmail.com>
2023-10-22 19:40:49 -0300

    RED - Modificación de productos

---
+++ Codigo/Backend/SpecFlowProject/StepDefinitions/UpdateProductStepDefinitions.cs
@@ -0,0 +1,273 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject.StepDefinitions
+{
+    [Binding]
+    public class UpdateProductStepDefinitions
+    {
+        [Given(@"que existe un producto con código numérico (.*)")]
+        public void GivenQueExisteUnProductoConCodigoNumerico(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"se ingresa en el escenario_UI un código numérico de (.*)")]
+        public void GivenSeIngresaEnElEscenario_UIUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"se ingresa en el escenario_UI un nombre de producto ""([^""]*)""")]
+        public void GivenSeIngresaEnElEscenario_UIUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"se ingresa en el escenario_UI una descripción ""([^""]*)""")]
+        public void GivenSeIngresaEnElEscenario_UIUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"se coloca en el escenario_UI un precio de (.*)")]
+        public void GivenSeColocaEnElEscenario_UIUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"presiono el botón de modificación")]
+        public void WhenPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"se modifica la información del producto correctamente")]
+        public void ThenSeModificaLaInformacionDelProductoCorrectamente()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UII se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UIISeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UII ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UIIIngresaUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UII ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UIIIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UII coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UIIColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UII presiono el botón de modificación")]
+        public void WhenEnElEscenario_UIIPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UII no se modifica la información del producto")]
+        public void ThenEnElEscenario_UIINoSeModificaLaInformacionDelProducto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UIII se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UIIISeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIII ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UIIIIngresaUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIII ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UIIIIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIII coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UIIIColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UIII presiono el botón de modificación")]
+        public void WhenEnElEscenario_UIIIPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UIII no se modifica la información del producto")]
+        public void ThenEnElEscenario_UIIINoSeModificaLaInformacionDelProducto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UIV no existe un producto con código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UIVNoExisteUnProductoConCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UIV se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UIVSeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIV ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UIVIngresaUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIV ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UIVIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UIV coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UIVColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UIV presiono el botón de modificación")]
+        public void WhenEnElEscenario_UIVPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UIV no se modifican los datos del producto porque no existe")]
+        public void ThenEnElEscenario_UIVNoSeModificanLosDatosDelProductoPorqueNoExiste()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UV se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UVSeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UV ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIngresaUnNombreDeProducto(string shampooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UV ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UV coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UVColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UV presiono el botón de modificación")]
+        public void WhenEnElEscenario_UVPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UV no se modifica la información del producto")]
+        public void ThenEnElEscenario_UVNoSeModificaLaInformacionDelProducto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UVI se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UVISeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVI ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIIngresaUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVI ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVI coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UVIColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UVI presiono el botón de modificación")]
+        public void WhenEnElEscenario_UVIPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UVI no se modifica la información del producto")]
+        public void ThenEnElEscenario_UVINoSeModificaLaInformacionDelProducto()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"que en el escenario_UVII se ingresa un código numérico de (.*)")]
+        public void GivenQueEnElEscenario_UVIISeIngresaUnCodigoNumericoDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVII ingresa un nombre de producto ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIIIngresaUnNombreDeProducto(string shampoo)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVII ingresa una descripción ""([^""]*)""")]
+        public void GivenEnElEscenario_UVIIIngresaUnaDescripcion(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"en el escenario_UVII coloca un precio de (.*)")]
+        public void GivenEnElEscenario_UVIIColocaUnPrecioDe(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"en el escenario_UVII presiono el botón de modificación")]
+        public void WhenEnElEscenario_UVIIPresionoElBotonDeModificacion()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"en el escenario_UVII no se modifica la información del producto")]
+        public void ThenEnElEscenario_UVIINoSeModificaLaInformacionDelProducto()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
